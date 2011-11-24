f31b19179b2fc1c3bb99f328e5f37f58154f4a9f
Dave Simons <dave@inuits.be>
2011-11-24 23:39:49 +0100

    no status check in initscript for puppet on arch
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/service.pp
+++ manifests/classes/service.pp
@@ -3,6 +3,10 @@ class puppet::service {
     'puppet':
       ensure => stopped,
       enable => false,
+      hasstatus => $::operatingsystem ? {
+        default   => undef,
+        archlinux => false,
+      },
       path => $::operatingsystem ? {
         default => undef,
         archlinux => '/etc/rc.d',
