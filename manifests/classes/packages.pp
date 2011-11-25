814386d71b02301084eb9464a20de44bbcb4f58b
Dave Simons <dave@inuits.be>
2011-11-25 10:57:54 +0100

    restructure code to make it work when the yaourt::pkg def isn't included
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/packages.pp
+++ manifests/classes/packages.pp
@@ -1,14 +1,15 @@
 class puppet::packages {
-  @package {
-    'puppet':;
-  }
-
-  @yaourt::pkg {
-    'puppet':;
-  }
-
   case $::operatingsystem {
-    default:   { realize(Package['puppet']) }
-    archlinux: { realize(Yaourt::Pkg['puppet']) }
+    default: {
+      package {
+        'puppet':;
+      }
+    }
+
+    archlinux: {
+      yaourt::pkg {
+        'puppet':;
+      }
+    }
   }
 }
