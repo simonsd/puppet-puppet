b1746faca61589fd5d805e70ee2ee42425ae27d1
Dave Simons <dave@inuits.be>
2011-11-25 11:53:26 +0100

    add some ownership details to puppet.conf file resource
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/config.pp
+++ manifests/classes/config.pp
@@ -1,6 +1,9 @@
 class puppet::config {
   file {
     '/etc/puppet/puppet.conf':
+      owner => root,
+      group => root,
+      mode => 0644,
       content => template('puppet/puppet.conf.erb');
   }
 }
